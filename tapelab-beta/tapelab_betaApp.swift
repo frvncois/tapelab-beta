031980c0aa92045ce74d146b4a0cac36515e8c71
Frvncois <hello@frvncois.com>
2025-11-05 17:40:20 -0500

    Initial Commit

---
+++ tapelab-beta/tapelab_betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tapelab_betaApp.swift
+//  tapelab-beta
+//
+//  Created by Francois Lemieux on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct tapelab_betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tapelab-betaTests/tapelab_betaTests.swift
@@ -0,0 +1,17 @@
+//
+//  tapelab_betaTests.swift
+//  tapelab-betaTests
+//
+//  Created by Francois Lemieux on 5/11/25.
+//
+
+import Testing
+@testable import tapelab_beta
+
+struct tapelab_betaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
